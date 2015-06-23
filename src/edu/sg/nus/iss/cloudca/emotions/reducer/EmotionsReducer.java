65450c7b1ec4b5f76a209a444e0f72e6c672809f
Sugesh <ks.sugesh@yahoo.com>
2015-06-23 16:54:14 -0700

    Add top tweets logic to reducer

--- src/edu/sg/nus/iss/cloudca/emotions/reducer/EmotionsReducer.java
+++ src/edu/sg/nus/iss/cloudca/emotions/reducer/EmotionsReducer.java
@@ -1,9 +1,11 @@
 package edu.sg.nus.iss.cloudca.emotions.reducer;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.apache.commons.collections4.queue.CircularFifoQueue;
 import org.apache.hadoop.mapreduce.Reducer;
-
 import edu.sg.nus.iss.cloudca.emotions.dto.EmotionsDataKey;
 import edu.sg.nus.iss.cloudca.emotions.dto.EmotionsDataValue;
 
@@ -18,7 +20,19 @@ public class EmotionsReducer extends Reducer<EmotionsDataKey, EmotionsDataValue,
 	@Override
 	protected void reduce(EmotionsDataKey key, Iterable<EmotionsDataValue> values, Context context)
 					throws IOException, InterruptedException {
-		key.getName();
+		Map<String,CircularFifoQueue<EmotionsDataValue>> map = new HashMap<String,CircularFifoQueue<EmotionsDataValue>> ();
+		for(EmotionsDataValue value : values) {
+			String keyString = value.getHappinessIndexText().toString();
+			CircularFifoQueue<EmotionsDataValue> topTweetsForCategory = map.get(keyString);
+			if(topTweetsForCategory == null) {
+				topTweetsForCategory = new CircularFifoQueue<EmotionsDataValue>(10);
+				map.put(keyString, topTweetsForCategory);
+			}
+			if(topTweetsForCategory.isEmpty() || topTweetsForCategory.peek().getIndicoValue().get()<value.getIndicoValue().get()) {
+				topTweetsForCategory.add(value);
+			}
+		}
+		System.out.println("Reduced");
 	}
 	
 	/**
